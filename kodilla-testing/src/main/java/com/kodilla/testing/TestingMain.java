fe8b735bd106df9d3cc65e1f524e02d6baff83d7
nasz_login_do_github <nasz_email_do_github>
2020-04-09 09:46:59 +0200

    Zadanie 6.2 Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calcurator6z2.Calculator6z2;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+
+        System.out.println("Moduł 6.2 Wprowadzenie to testowania oprogramowania");
+        Calculator6z2 calculator = new Calculator6z2();
+        int addResult = calculator.addAB(1,4);
+         if (addResult==5) {
+           System.out.println("test a+b OK");
+            } else {
+           System.out.println("Error!");
+         }
+        int subResult = calculator.subtractAB(9,4);
+        if (subResult==5) {
+            System.out.println("test a-b OK");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calcurator6z2/Calculator6z2.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calcurator6z2;
+
+public class Calculator6z2 {
+
+    public int addAB(int a, int b) {
+        return  a + b;
+    }
+
+    public int subtractAB(int a, int b) {
+        return  a - b;
+    }
+}

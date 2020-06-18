9bad617003af47f7f914dacf5c12182f12aacbff
nasz_login_do_github <nasz_email_do_github>
2020-06-18 21:08:17 +0200

    problemy z konfiguracją MODUL 14

--- kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -3,6 +3,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootAplication;
 
 @SpringBootAplication
+
+
+
+
+
+
 public class SpringRunner {
 
     public static void main(String[] args) {

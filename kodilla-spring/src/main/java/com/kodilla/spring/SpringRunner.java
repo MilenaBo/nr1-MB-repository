1c683ebde503d794192773c5dd3b5fdf53729fad
nasz_login_do_github <nasz_email_do_github>
2020-06-18 12:15:21 +0200

    moduł 14 PROBLEMY

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -0,0 +1,11 @@
+package com.kodilla.spring;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootAplication;
+
+@SpringBootAplication
+public class SpringRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRunner.class, args);
+    }
+}

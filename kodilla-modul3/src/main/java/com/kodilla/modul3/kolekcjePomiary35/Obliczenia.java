8a26d63959c2a2b241c527aa995197e665fb38a9
MilenaBo <mboboran@siseth.com>
2020-11-27 21:22:10 +0100

    30.3 zadanie Wywołanie procedury UpdateBestsellers z poziomu Javy

---
+++ kodilla-modul3/src/main/java/com/kodilla/modul3/kolekcjePomiary35/Obliczenia.java
@@ -0,0 +1,20 @@
+package com.kodilla.modul3.kolekcjePomiary35;
+
+class Obliczenia {
+    public  static  void main (String[] args) {
+        Double d = 5.3d;
+        Integer a = 1;
+        Integer i = 3;
+        int z;
+        z = 1;
+        int x = 2;
+        int c;
+        c = z + x;
+          // i=i+d;
+
+        String s=s1="0";
+
+        System.out.print("*************wynik"+);
+
+    }
+}
